6ae5b00919d3bcc4b29b3116bb386fccb008e486
Jorge Sainz <bodybluebloody@gmail.com>
2022-02-23 16:08:27 +0100

    Added styling for medium breakpoint. Mobile size breakpoint pending

--- src/components/NavBar/components/Navigation.js
+++ src/components/NavBar/components/Navigation.js
@@ -3,8 +3,8 @@ import Logo from '../assets/logo.svg'
 const Navigation = () => {
   return (
     <div className='nav-container__left'>
-      <a href='/' target='_blank'>
-        <Logo width='140' height='20' />
+      <a className='logo-container' href='/' target='_blank'>
+        <Logo className='logo-container__logo' />
       </a>
       <ul className='nav-links'>
         <li className='nav-links__element'><a href='/' target='_blank'>Collections</a></li>
--- src/components/Product/ProductDetails/components/Details.js
+++ src/components/Product/ProductDetails/components/Details.js
@@ -5,12 +5,14 @@ const Details = ({ product }) => {
       <h1 className='product-details__name'>{product.name}</h1>
       <p className='product-details__description'>{product.description}</p>
       <div className='product-details__price-container'>
-        <h2 className='price'>{product.price}</h2>
-        {product.hasDiscount
-          ? <span className='discount'>{product.discount}</span>
-          : null}
+        <div className='current-price'>
+          <h2 className='current-price__value'>{product.price}</h2>
+          {product.hasDiscount
+            ? <span className='current-price__discount'>{product.discount}</span>
+            : null}
+        </div>
+        <h3 className='product-details__previous-price'>{product.prevPrice}</h3>
       </div>
-      <h3 className='product-details__previous-price'>{product.prevPrice}</h3>
     </div>
   )
 }
