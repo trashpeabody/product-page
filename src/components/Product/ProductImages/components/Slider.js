40d80eb835342b251c415644d815558c0df3e6b0
Jorge Sainz <bodybluebloody@gmail.com>
2022-02-26 18:43:34 +0100

    Added swiping gestures to slider

--- src/components/Product/ProductImages/components/Slider.js
+++ src/components/Product/ProductImages/components/Slider.js
@@ -1,7 +1,11 @@
+import { useSwipeable } from 'react-swipeable'
+import { useContext } from 'react'
+import Context from '../../../../context/ImageContext'
 import Image1 from '../assets/image-product-1.jpg'
 import Image2 from '../assets/image-product-2.jpg'
 import Image3 from '../assets/image-product-3.jpg'
 import Image4 from '../assets/image-product-4.jpg'
+import { doNavigation } from '../handlers/Images'
 
 const Slider = () => {
   const handleClick = () => {
@@ -9,8 +13,20 @@ const Slider = () => {
     lightbox.style.display = 'flex'
   }
 
+  const { thumbnails, setThumbnails } = useContext(Context)
+
+  const handlers = useSwipeable({
+    onSwipedRight: () => {
+      doNavigation(true, thumbnails, setThumbnails)
+    },
+    onSwipedLeft: () => {
+      doNavigation(false, thumbnails, setThumbnails)
+    },
+    preventDefaultTouchmoveEvent: false
+  })
+
   return (
-    <div className='slider-container'>
+    <div className='slider-container' {...handlers}>
       <img className='main-image pointer' src={Image1} onClick={handleClick} alt='Product main image' />
       <img className='main-image pointer' src={Image2} onClick={handleClick} alt='Product main image' />
       <img className='main-image pointer' src={Image3} onClick={handleClick} alt='Product main image' />
