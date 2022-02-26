ea816a6c0b5bdf670525c02b80fec7677b870d9a
Jorge Sainz <bodybluebloody@gmail.com>
2022-02-26 18:08:14 +0100

    Added slider to lightbox and product images

--- src/components/Product/ProductImages/components/ThumbnailContainer.js
+++ src/components/Product/ProductImages/components/ThumbnailContainer.js
@@ -1,13 +1,32 @@
+import { useContext } from 'react'
+import Context from '../../../../context/ImageContext'
 import Thumbnail from './Thumbnail'
-import { handleClick } from '../handlers/Images'
 
-const ThumbnailContainer = ({ thumbnails, setThumbnails, setImage }) => {
+const ThumbnailContainer = () => {
+  const { thumbnails, setThumbnails } = useContext(Context)
+
+  const handleClick = (index) => {
+    return () => {
+      for (const image of document.getElementsByClassName('main-image')) {
+        const translation = 100 * index
+        image.style.transform = `translateX(-${translation}%)`
+      }
+      setThumbnails(
+        thumbnails.map(
+          thumb => thumb.index === index
+            ? { ...thumb, isSelected: true }
+            : { ...thumb, isSelected: false }
+        )
+      )
+    }
+  }
+
   return (
     <div className='thumbnails-container'>
       {thumbnails.map(thumbnail =>
         <Thumbnail
           key={thumbnail.index}
-          handleClick={handleClick(thumbnails, setThumbnails, setImage, thumbnail.index)}
+          handleClick={handleClick(thumbnail.index)}
           thumbnail={thumbnail}
         />
       )}
